ec85f16ae4e9edb073349ab9e54537496527ba4a
Aywin <AywinK@hotmail.com>
2023-09-25 19:15:23 +0100

    wip reducer initialisation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState, useReducer } from 'react';
 
 import './App.css';
 
@@ -10,7 +10,28 @@ import PauseRoundedIcon from '@mui/icons-material/PauseRounded';
 import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "RESET":
+
+      return init();
+
+    default:
+      return state;
+  }
+}
+
+const init = () => ({
+  isRunning: false,
+  breakLength: 5,
+  sessionLength: 25,
+  timeLeft: 1500,
+  sessionType: "Focus"
+});
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, null, init)
 
   const [isPlaying, setIsPlaying] = useState(false);
 
@@ -32,21 +53,21 @@ function App() {
     <div className="App">
       <audio ref={audioRef} id="beep" src={beep} preload="auto" type="audio/mp3"></audio>
       <h1 className="title">Tic Toc Tomato</h1>
-      <h2 id="timer-label">Focus/Break</h2>
-      <span id="time-left">25:00</span>
+      <h2 id="timer-label">{state.sessionType}</h2>
+      <span id="time-left">{`${Math.floor(state.timeLeft / 60)}:`}{state.timeLeft % 60 < 10 ? `0${state.timeLeft % 60}` : state.timeLeft % 60}</span>
       <button className="btn" onClick={handlePlayAudio} id="start_stop"><PlayArrowRoundedIcon fontSize="large" /><PauseRoundedIcon fontSize="large" /></button>
       <div id="secondary-controls">
         <div id="session-controls">
           <h3 id="session-label">Session Duration</h3>
           <button className="btn" id="session-decrement"><RemoveRoundedIcon fontSize="large" /></button>
-          <span id="session-length">25</span>
+          <span id="session-length">{state.sessionLength}</span>
           <button className="btn" id="session-increment"><AddRoundedIcon fontSize="large" /></button>
         </div>
         <button className="btn" id="reset"><RestartAltRoundedIcon fontSize="large" /></button>
         <div id="break-controls">
           <h3 id="break-label">Break Duration</h3>
           <button className="btn" id="break-decrement"><RemoveRoundedIcon fontSize="large" /></button>
-          <span id="break-length">5</span>
+          <span id="break-length">{state.breakLength}</span>
           <button className="btn" id="break-increment"><AddRoundedIcon fontSize="large" /></button>
         </div>
       </div>
